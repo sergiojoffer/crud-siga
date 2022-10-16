8ae17a77384dd00f3ccfe5c3b85e5223b897e440
sergiojoffer <svsjoffer@gmail.com>
2022-10-16 10:40:16 -0400

    Segundo commit

--- src/app/cursos/curso-form/curso-form.component.ts
+++ src/app/cursos/curso-form/curso-form.component.ts
@@ -1,35 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import {ControlContainer, FormBuilder, FormGroup, NonNullableFormBuilder} from "@angular/forms";
-import {CursosService} from "../services/cursos.service";
-import { Curso } from "../model/curso";
+import {
+  ControlContainer,
+  FormBuilder,
+  FormGroup,
+  NonNullableFormBuilder,
+} from '@angular/forms';
+import { CursosService } from '../services/cursos.service';
+import { Curso } from '../model/curso';
 
 @Component({
   selector: 'app-curso-form',
   templateUrl: './curso-form.component.html',
-  styleUrls: ['./curso-form.component.scss']
+  styleUrls: ['./curso-form.component.scss'],
 })
 export class CursoFormComponent implements OnInit {
-
   form = this.formBuilder.group({
     _id: [''],
     name: [''],
-    category: ['']
+    category: [''],
   });
 
+  constructor(
+    private formBuilder: NonNullableFormBuilder,
+    private service: CursosService
+  ) {}
 
-  constructor(private formBuilder: NonNullableFormBuilder, private service: CursosService
-              ) {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit() {
+    //
+    this.service
+      .save(this.form.value)
+      .subscribe((result) => console.log(result));
+    console.log(this.form.value);
   }
 
- onSubmit() {
-  //
-   this.service.save(this.form.value).subscribe(result => console.log(result));
-  //console.log(this.form.value);
- }
-
- onCancel() {
-
- }
+  onCancel() {}
 }
